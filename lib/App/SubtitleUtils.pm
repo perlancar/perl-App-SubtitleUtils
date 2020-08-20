ce0ac7c09f3fb79cfbcf3f131fd7ae613626dcd0
perlancar (on pc-home) <perlancar@gmail.com>
2020-08-20 18:38:56 +0700

    Initial commit

---
+++ lib/App/SubtitleUtils.pm
@@ -0,0 +1,20 @@
+package App::SubtitleUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to subtitles
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut

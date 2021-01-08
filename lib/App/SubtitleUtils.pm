424bd4751bded173dd30cd05babf3143bf2a81c5
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-08 07:30:04 +0700

    [doc] Add History section

--- lib/App/SubtitleUtils.pm
+++ lib/App/SubtitleUtils.pm
@@ -15,6 +15,13 @@ This distributions provides the following command-line utilities:
 # INSERT_EXECS_LIST
 
 
+=head1 HISTORY
+
+Most of them are scripts I first wrote in 2003 and first packaged as CPAN
+distribution in late 2020. They need to be rewritten to properly use
+L<Getopt::Long> etc; someday.
+
+
 =head1 SEE ALSO
 
 =cut
